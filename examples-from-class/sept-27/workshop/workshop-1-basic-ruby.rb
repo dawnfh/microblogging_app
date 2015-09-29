fff5ca14b64c460fe78e9351284f4836bfc0b3d6
Orlando Caraballo <orlandocaraballo@Orlandos-MacBook-Pro.local>
2015-09-28 22:17:31 -0400

    Added files.

---
+++ examples-from-class/sept-27/workshop/workshop-1-basic-ruby.rb
@@ -0,0 +1,36 @@
+def concat_for_murica(phrase)
+	phrase << "Only In America!"
+end
+
+# Cheeseburgers exist: Only in America
+puts concat_for_murica("Cheeseburgers exist: ")
+
+def mad_max(numbas)
+	max = 0
+
+	numbas.each do |numba|
+		if numba > max
+			max = numba
+		end
+	end
+
+	max
+end
+
+# 9000
+puts mad_max([9000, -3, 45])
+
+
+def arrays_to_hash(keys, values)
+	hash = {}
+	i = 0
+
+	keys.each do |key|
+		hash[key] = values[i]
+		i = i + 1
+	end
+
+	hash
+end
+
+puts arrays_to_hash([:I, :she, :your], ["like food", "does too", "move sailor"])
