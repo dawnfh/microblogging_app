c72945e37cb973213c7833afac6a54da85543898
Orlando Caraballo <orlando@nycda.com>
2015-10-06 09:03:36 -0400

    deleted old files

--- examples-from-class/oct-6/activerecord-sinatra/db/migrate/20151006012343_create_users_table.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsersTable < ActiveRecord::Migration
-  def change
-    create_table :users do |t|
-      t.string :email
-      t.string :fname
-      t.datetime :birthday
-    end
-  end
-end
