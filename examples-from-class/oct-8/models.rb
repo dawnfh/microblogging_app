f27cf3f3004532e4957aebd0dcb216309557f0bf
Orlando Caraballo <orlando@nycda.com>
2015-10-08 08:40:17 -0400

    moved some files around

---
+++ examples-from-class/oct-8/models.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  # the way we describe follows is as a list
+  #   of people that have followed you
+  # when we want to see a list of all the follows we have
+  #   we must tell it to look at the followee_id foreign key
+  has_many :follows, foreign_key: :followee_id
+end
+
+class Follow < ActiveRecord::Base
+  # when we want to see a list of all follows,
+  #   we must indicate that when we do a <follow object>.user
+  #   it needs to look at the follower_id foreign key
+  belongs_to :user, foreign_key: :follower_id
+end
